bf72ad2fb70d67fb1e12420c47a9ad3f0c57eeb8
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2018-08-19 12:57:30 +0700

    add /sports endpoint, add start script

--- index.js
+++ index.js
@@ -11,39 +11,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/sports", (req, res) => res.send({ sports: api.getAllSports() }));
 
 app.get("/schedules", (req, res, next) => {
-    const schedules = api.getAllSchedule(req.query);
-    res.send({
-        status: "ok",
-        data: schedules
-    });
+  const schedules = api.getAllSchedule(req.query);
+  res.send({
+    status: "ok",
+    data: schedules
+  });
 });
 
 app.get("/countries", (req, res, next) => {
-    const countries = api.getCountry();
-    res.send({
-        status: "ok",
-        data: countries
-    });
+  const countries = api.getCountry();
+  res.send({
+    status: "ok",
+    data: countries
+  });
 });
 
 app.get("/athlete", (req, res, next) => {
-    const country = req.query.country;
+  const country = req.query.country;
 
-    if (country) {
-        const athletesByCountry = api.getAllAthleteByCountry(country);
-        res.send({
-            status: "ok",
-            data: athletesByCountry
-        });
-    } else {
-        const athletes = api.getAllAthlete();
-        res.send({
-            status: "ok",
-            data: athletes
-        });
-    }
+  if (country) {
+    const athletesByCountry = api.getAllAthleteByCountry(country);
+    res.send({
+      status: "ok",
+      data: athletesByCountry
+    });
+  } else {
+    const athletes = api.getAllAthlete();
+    res.send({
+      status: "ok",
+      data: athletes
+    });
+  }
 });
 
-app.listen(PORT, () => console.log(`asian-games-api listening on port ${PORT}!`));
+app.listen(PORT, () =>
+  console.log(`asian-games-api listening on port ${PORT}!`)
+);
--- utils/api.js
+++ utils/api.js
@@ -1,41 +1,48 @@
 "use strict";
 const request = require("request-promise");
 const _ = require("lodash");
-const scheduleByCountry = require('./scheduleByCountry.json');
-const scheduleBySport = require('./scheduleBySport.json');
-const athleteList = require('./athleteList.json');
+const scheduleByCountry = require("./scheduleByCountry.json");
+const scheduleBySport = require("./scheduleBySport.json");
+const athleteList = require("./athleteList.json");
 
 module.exports = {
-    getAllSchedule: function (query) {
-        let listSchedule = [];
-        let schedules = {};
-        if (query.sport) {
-            schedules = scheduleBySport[query.sport].scheduleByDay;
-        } else if (query.country) {
-            schedules = scheduleByCountry[query.country].scheduleByDay;
-        }
-        for (let date in schedules) {
-            listSchedule = _.concat(listSchedule, schedules[date]);
-        }
-        listSchedule.sort(function compare(a, b) {
-            var dateA = new Date(a.time);
-            var dateB = new Date(b.time);
-            return dateA - dateB;
-        });
-        return listSchedule;
-    },
-    getCountry: function () {
-        let country = {};
-        for (let c in scheduleByCountry) {
-            country[c] = scheduleByCountry[c].name;
-        }
-        return country;
-    },
-    getAllAthlete: function () {
-        return athleteList;
-    },
-    getAllAthleteByCountry: function (country) {
-        let listCountryAthlete = athleteList[country].countryAthletes;
-        return listCountryAthlete;
+  getAllSports: function() {
+    const sports = Object.keys(scheduleBySport).map((key, index) => ({
+      code: key,
+      title: scheduleBySport[key].title
+    }));
+    return sports;
+  },
+  getAllSchedule: function(query) {
+    let listSchedule = [];
+    let schedules = {};
+    if (query.sport) {
+      schedules = scheduleBySport[query.sport].scheduleByDay;
+    } else if (query.country) {
+      schedules = scheduleByCountry[query.country].scheduleByDay;
     }
-}
+    for (let date in schedules) {
+      listSchedule = _.concat(listSchedule, schedules[date]);
+    }
+    listSchedule.sort(function compare(a, b) {
+      var dateA = new Date(a.time);
+      var dateB = new Date(b.time);
+      return dateA - dateB;
+    });
+    return listSchedule;
+  },
+  getCountry: function() {
+    let country = {};
+    for (let c in scheduleByCountry) {
+      country[c] = scheduleByCountry[c].name;
+    }
+    return country;
+  },
+  getAllAthlete: function() {
+    return athleteList;
+  },
+  getAllAthleteByCountry: function(country) {
+    let listCountryAthlete = athleteList[country].countryAthletes;
+    return listCountryAthlete;
+  }
+};
